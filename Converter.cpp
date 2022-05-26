59a6d61ed65461ff56dc4dc3f92ff49d45fe6273
1st_award <54760813+1st-award@users.noreply.github.com>
2022-05-27 00:33:22 +0900

    Converter 클래스 수정

--- Converter.cpp
+++ Converter.cpp
@@ -4,6 +4,7 @@
 
 #include "Converter.h"
 #include "raylib.h"
+#include <iostream>
 
 int mapXLength;
 int mapZLength;
@@ -11,11 +12,12 @@ int mapZLength;
 Vector3 Converter::translateToAbsolute(int relativePosX, int relativePosZ) {
     float absolutePosX = 0.5f - mapXLength / 2.0f + relativePosX;
     float absolutePosZ = 0.5f - mapZLength / 2.0f + relativePosZ;
-    Vector3 absoluteVector{(float) relativePosX, 0.0f, (float) relativePosZ};
+    Vector3 absoluteVector{absolutePosX, 0.0f, absolutePosZ};
     return absoluteVector;
 }
 
 void Converter::setMapLength(int XLength, int ZLength) {
     mapXLength = XLength;
     mapZLength = ZLength;
+    std::cout << mapZLength << mapXLength;
 }
